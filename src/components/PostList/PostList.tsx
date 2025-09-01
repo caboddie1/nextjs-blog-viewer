89c82b9589e3dba466981754f22cb9e4a1b9d2ec
Cameron <cameron_b@hotmail.co.uk>
2025-09-01 23:38:08 +0100

    feat: add PostList component with story and test

---
+++ src/components/PostList/PostList.tsx
@@ -0,0 +1,19 @@
+import { Post } from "@/types/post";
+import PostCard from "../PostCard/PostCard";
+
+interface Props {
+    posts: Post[];
+}
+
+export default function PostList({ posts }: Props) {
+    return (
+        <div className="row">
+            <h2 className="mb-3">Posts</h2>
+            {posts.map((post) => (
+                <div key={post.id} className="col-xl-3 col-md-6 col-sm-12 mb-4">
+                    <PostCard post={post} />
+                </div>
+            ))}
+        </div>
+    );
+}
